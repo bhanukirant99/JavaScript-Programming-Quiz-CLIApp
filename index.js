672476e51011d412bbed3f98fdd1cf7670c91898
bhanukirant99 <bhanukirant99@gmail.com>
2020-12-15 10:51:13 +0530

    refactor: aligning the code in index.js

--- index.js
+++ index.js
@@ -28,37 +28,38 @@ function play(question, answer) {
     console.log("--------------------------\n");
 }
 
-  const questions = [{
-    question: "What do we use to print a value to the console? ",
-    answer: "console.log()"
-  }, {
-    question: "Which built-in method combines the text of two strings and returns a new string? ",
-    answer: "concat()"
-  }, {
-    question: "Which function of Number object returns the number's value? ",
-    answer: "valueOf()"
-  }, {
-    question: "Which function of String object splits a String object into an array of strings by separating the string into substrings? ",
-    answer: "split()"
-  }, {
-    question: "Which function of String object returns the calling string value converted to upper case? ",
-    answer: "toUpperCase()"
-  }, {
-    question: "Which function of Array object removes the last element from an array and returns that element? ",
-    answer: "pop()"
-  }, {
-    question: "Which function of Array object adds one or more elements to the front of an array and returns the new length of the array? ",
-    answer: "unshift()"
-  }]
+const questions = [{
+  question: "What do we use to print a value to the console? ",
+  answer: "console.log()"
+}, {
+  question: "Which built-in method combines the text of two strings and returns a new string? ",
+  answer: "concat()"
+}, {
+  question: "Which function of Number object returns the number's value? ",
+  answer: "valueOf()"
+}, {
+  question: "Which function of String object splits a String object into an array of strings by separating the string into substrings? ",
+  answer: "split()"
+}, {
+  question: "Which function of String object returns the calling string value converted to upper case? ",
+  answer: "toUpperCase()"
+}, {
+  question: "Which function of Array object removes the last element from an array and returns that element? ",
+  answer: "pop()"
+}, {
+  question: "Which function of Array object adds one or more elements to the front of an array and returns the new length of the array? ",
+  answer: "unshift()"
+}]
 
-  while(playAgain.toLowerCase() === 'y') {
-    playAgain = 'n';
-    for (var i = 0; i < questions.length; i++) {
-      play(questions[i].question, questions[i].answer);
-    }
-  
-    console.log("Final score is ", score);
-    score = 0;
-    playAgain = readlineSync.question('Do you wanna play again (y/n)? ');
-  
+
+while(playAgain.toLowerCase() === 'y') {
+  playAgain = 'n';
+  for (var i = 0; i < questions.length; i++) {
+    play(questions[i].question, questions[i].answer);
   }
+
+  console.log("Final score is ", score);
+  score = 0;
+  playAgain = readlineSync.question('Do you wanna play again (y/n)? ');
+
+}
